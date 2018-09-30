a7b2171bf249b1d0a6688f7b71596aff420b82a7
lambdaq <lambdaq@gmail.com>
2018-09-30 19:34:24 +0800

    fix urllib and return correct start/end for issue

--- keeper.py
+++ keeper.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import os, sys
-import urllib2, json
+import json
+from urllib import request
 
 
 ENTRY = os.environ.get('GITLAB_URL')
@@ -18,9 +19,11 @@ class Gitlab:
         self.gitlab_url, self.gitlab_token = gitlab_url, gitlab_token
 
     def req_api(self, path):
-        r = urllib2.urlopen(urllib2.Request(
-            f'{self.gitlab_url}{path}',
-            headers={'PRIVATE-TOKEN', self.gitlab_token}))
+        r = request.urlopen(request.Request(f'{self.gitlab_url}{path}', headers={
+            'PRIVATE-TOKEN': self.gitlab_token,
+            'Access-Control-Request-Headers': 'private-token',
+            'User-Agent': 'gitlab-issue-keeper',
+        }), timeout=2)
         return json.loads(r.read())
 
     def get_issue_notes(self, iid):
@@ -29,12 +32,20 @@ class Gitlab:
 
     def get_doing_close_date(self, iid):
         label_id = self.labels_map[DOING_LABEL]
-        starts = [
-            x for x in self.get_issue_notes(iid)
-            if x['system'] and x['body'] == f'added ~{label_id} label']
-        ends = [
-            x for x in self.get_issue_notes(iid)
-            if x['system'] and x['body'] in ['closed', f'removed ~{label_id} label']]
+        issue_notes = list(self.get_issue_notes(iid))
+        starts = sorted([x for x in issue_notes if
+            x['system'] and x['body'] == f'added ~{label_id} label'  # noqa
+        ], key=lambda x: x['id']) or issue_notes[-2:]
+
+        ends1 = sorted([x for x in issue_notes if
+            x['system'] and x['body'] == 'closed'  # noqa
+        ], key=lambda x: x['id'])
+
+        ends2 = sorted([x for x in issue_notes if
+            x['system'] and x['body'] == f'removed ~{label_id} label'  # noqa
+        ], key=lambda x: x['id'])
+
+        return starts[0]['updated_at'], min(ends1[-2:] + ends2[-2:])['updated_at']
 
     def list_issues(self):
         r = self.req_api('/api/v4/projects/228/issues?page=1&per_page=100&state=opened')
