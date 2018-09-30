924e4f96748e2ea932256eac6cbdd2284b220e8d
lambdaq <lambdaq@gmail.com>
2018-09-30 20:16:59 +0800

    edit issue for Gantt tracking

--- keeper.py
+++ keeper.py
@@ -3,14 +3,18 @@
 import os, sys
 import json
 from urllib import request
+from urllib.parse import urlencode
 
 
 ENTRY = os.environ.get('GITLAB_URL')
 TOKEN = os.environ.get('GITLAB_TOKEN')
 DOING_LABEL = 'Doing'
+GANTT_START = 'GanttStart:'
+GANTT_END = 'GanttDue:'
 
 # ToDo: get project by url
-# ToDO: by milestone
+# ToDO: group by milestone
+# ToDo: issue start date by /spend
 
 
 class Gitlab:
@@ -19,13 +23,19 @@ class Gitlab:
         self.gitlab_url, self.gitlab_token = gitlab_url, gitlab_token
         self.labels = {}
 
-    def req_api(self, path):
-        r = request.urlopen(request.Request(f'{self.gitlab_url}{path}', headers={
+    def req_api(self, path, data=None):
+        headers = {
             'PRIVATE-TOKEN': self.gitlab_token,
             'Access-Control-Request-Headers': 'private-token',
             'User-Agent': 'gitlab-issue-keeper',
-        }), timeout=2)
-        return json.loads(r.read())
+        }
+        if data is None:
+            r = request.Request(f'{self.gitlab_url}{path}', headers=headers)
+        else:
+            params = urlencode(data)
+            # stupid RESTful
+            r = request.Request(f'{self.gitlab_url}{path}?{params}', headers=headers, method='PUT')
+        return json.loads(request.urlopen(r, timeout=2).read())
 
     def get_issue_notes(self, iid):
         r = self.req_api(f'/api/v4/projects/228/issues/{iid}/notes')
@@ -56,6 +66,18 @@ class Gitlab:
             iid = issue['iid']
             start, end = self.get_doing_close_date(issue['iid'])
             print('%04d: %s %s' % (iid, start, end))
+            gantt_str = ''
+            if start:
+                gantt_str = '%s\n%s%s' % (gantt_str, GANTT_START, start)
+            if end:
+                gantt_str = '%s\n%s%s' % (gantt_str, GANTT_END, end)
+            if gantt_str:
+                # remove old str
+                desc_back = '\n'.join(
+                    l for l in issue['description'].splitlines() if
+                    not (l.startswith(GANTT_START) or l.startswith(GANTT_END)))
+                desc = '%s\n\n<!--\n下面是issue耗时跟踪不要删\n%s\n-->' % (desc_back, gantt_str)
+                r = self.req_api(f'/api/v4/projects/228/issues/{iid}', {"description": desc})
 
     @property
     def labels_map(self):
